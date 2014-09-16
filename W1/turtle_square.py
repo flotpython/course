6b1d8b4cec5e34ffe8e2118b16bffae48db2ca51
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-09-16 15:36:51 +0200

    wrote a little more elaborate program using turtle so the hackers will have something to play with during the first week

--- W1/turtle_square.py
+++ W1/turtle_square.py
@@ -3,7 +3,7 @@ import turtle
 
 
 def square (length):
-    "a procedure to draw a square of side length"
+    "have the turtle draw a square of side <length>"
     for side in range (4):
         turtle.forward (length)
         turtle.left (90)
@@ -12,6 +12,6 @@ def square (length):
 # initialize the turtle
 turtle.reset()
 # draw the square
-square (300)
+square (200)
 # wait for a user to click and then exit
 turtle.exitonclick()
---
+++ W1/turtle_squares.py
@@ -0,0 +1,55 @@
+# we need the turtle module
+import turtle
+
+# avoid calling range for each square
+sides = [ 'east', 'north', 'west', 'south' ]
+
+def square (the_turtle, length):
+    "have the turtle draw a square of side <length>"
+    for side in sides:
+        the_turtle.forward (length)
+        the_turtle.left (90)
+
+# initialize
+window = turtle.Screen()
+window.title ("Caroline et Chloe")
+
+# create first turtle
+caroline = turtle.Turtle ()
+caroline.color("hotpink")
+caroline.reset()
+
+# second turtle
+chloe = turtle.Turtle ()
+chloe.color("lightgreen")
+chloe.reset()
+
+# alternate : turtle, twist and square size
+contexts = ( ( caroline, 15, 100, ),
+             ( chloe, 60, 30 ),
+         )
+# initialize alternating contexts
+cycle = len (contexts)
+counter = -1
+
+# the callback triggered when a user clicks in x,y
+def clicked (x,y):
+    global counter
+    counter += 1
+    # alternate between the various contexts
+    ( turtle, twist, size) = contexts [ counter % cycle ]
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
+    turtle.left(twist)
+    square (turtle, size)
+
+# arm callback
+turtle.onscreenclick (clicked)
+
+# user can quit by typing 'q'
+turtle.onkey (turtle.bye,'q')
+turtle.listen()
+
+# read & dispatch events
+turtle.mainloop()
