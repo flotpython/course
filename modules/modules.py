2992c64276975e95f1cc67c80cc3112a2aee8c28
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-09-18 12:36:35 +0200

    modules.show_module can show only a part of the source code

--- modules/modules.py
+++ modules/modules.py
@@ -1,9 +1,10 @@
 import sys
 
-def show_module (module):
+def show_module (module, beg=None, end=None):
     """
     display the source code for a module
-    and more stuff
+    if beg is provided, listing starts with the first matching line
+    if end is provided, listing ends with the first matching line (excluded)
     """
     # check this is a module
     if type(module) is not type(sys):
@@ -12,11 +13,16 @@ def show_module (module):
     try:
         name = module.__name__
         file = module.__file__
+        state = True if not beg else False
         with open(module.__file__) as input:
             for line in input.readlines():
-                print line,
+                if beg and line.find(beg)>=0: state = True
+                if state and end and line.find(end)>=0: return
+                if state: print line,
     except AttributeError as e:
         print "show_module: Could not find module {}".format(module)
     except Exception as e:
         print "show_module: Could not find module {}".format(name)
+        import traceback
+        traceback.print_exc()
 
