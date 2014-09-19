aefe849b1fc3f3775e7671d7db614bf26a0b2c9a
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-09-19 12:42:42 +0200

    cosmetic improvements to show_module

--- modules/modules.py
+++ modules/modules.py
@@ -13,16 +13,26 @@ def show_module (module, beg=None, end=None):
     try:
         name = module.__name__
         file = module.__file__
-        state = True if not beg else False
+        # we start in showing mode unless beg was provided
+        showing = True if not beg else False
         with open(module.__file__) as input:
+            print "Fichier {}".format(module.__file__)
+            if not showing: 
+                print "<...>"
             for line in input.readlines():
-                if beg and line.find(beg)>=0: state = True
-                if state and end and line.find(end)>=0: return
-                if state: print line,
+                # turn on showing mode if beg string is seen
+                if beg and line.find(beg)>=0: 
+                    showing = True
+                # get out if in showing mode and end string is seen
+                if showing and end and line.find(end)>=0:
+                    print "<...>"
+                    return
+                # print this line if in showing mode
+                if showing: 
+                    print line,
     except AttributeError as e:
         print "show_module: Could not find module {}".format(module)
     except Exception as e:
-        print "show_module: Could not find module {}".format(name)
+        print "show_module: Could not show module {}".format(name)
         import traceback
         traceback.print_exc()
-
