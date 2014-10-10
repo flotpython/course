c16fb884b91e601097364a7cfb0d41e1a4610a07
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-10-10 19:06:41 +0200

    all exos now use Exercice

--- modules/corrections/w4_files.py
+++ modules/corrections/w4_files.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from corrections.exercice import correction_table, correction_table_1arg, exemple_table, exemple_table_1arg
+from exercice import Exercice, Exercice_1arg, Exercice_multiline
 
 import os
 
-# helpers
+#################### 
+# the function as it is specified does not fit in the 'exercice' framework
+# so here create helpers to make all this compliant
 def file_contents (filename):
     try:
         with open(filename) as input:
@@ -11,9 +13,8 @@ def file_contents (filename):
     except:
         return ""
 
-# the function as it is specified does not fit the correction_table model
 # this decorator does the trick to transform a student-written function
-# into something that correction_table can deal with
+# into something that exercice/correction_table can deal with
 def exercice_compliant (fun):
     def wrapped (in_name, out_name):
         # clean up output (in case the function does not create it
@@ -27,6 +28,30 @@ def exercice_compliant (fun):
         return file_contents (out_name)
     return wrapped
 
+### might end up in exercice if there's enough interest
+from IPython.display import HTML
+
+# on lance la fonction dans un fichier temporaire et on le montre
+def show_comptage (in_name, out_name, comptage, suffix):
+    out_name += suffix
+    comptage (in_name, out_name)
+    try:
+        input, output = file_contents (in_name), file_contents (out_name)
+        os.unlink(out_name)
+    except OSError:
+        print "Votre fonction ne semble pas crÃ©er le fichier de sortie"
+        return 
+    html = ""
+    html += "<table>"
+    html += "<tr><th>EntrÃ©e</th></tr>"
+    for line in input.split("\n"):
+        html += "<tr><td>{}</td></tr>".format(line)
+    html += "<tr><th>Sortie</th></tr>"
+    for line in output.split("\n"):
+        html += "<tr><td>{}</td></tr>".format(line)
+    html += "</table>"
+    return HTML(html)
+    
 ####################
 # comptage ()
 @exercice_compliant
@@ -51,37 +76,18 @@ comptage_inputs = [
     ('data/lorem_ipsum.txt', 'lorem_ipsum.out'),
 ]
 
-def correction_comptage (student_comptage):
-    # decorate before passing to the comparison engine
-    student_comptage = exercice_compliant(student_comptage)
-    return correction_table (student_comptage, comptage, comptage_inputs)
+class ExerciceComptage (Exercice):
 
-#
-from IPython.display import HTML
+    def correction (self, student_comptage):
+        # call the decorator on the student code
+        return Exercice.correction (self, exercice_compliant(student_comptage))
 
-# on lance la fonction dans un fichier temporaire et on le montre
-def show_comptage (in_name, out_name, comptage, suffix):
-    out_name += suffix
-    comptage (in_name, out_name)
-    try:
-        input, output = file_contents (in_name), file_contents (out_name)
-        os.unlink(out_name)
-    except OSError:
-        print "Votre fonction ne semble pas créer le fichier de sortie"
-        return 
-    html = ""
-    html += "<table>"
-    html += "<tr><th>Entrée</th></tr>"
-    for line in input.split("\n"):
-        html += "<tr><td>{}</td></tr>".format(line)
-    html += "<tr><th>Sortie</th></tr>"
-    for line in output.split("\n"):
-        html += "<tr><td>{}</td></tr>".format(line)
-    html += "</table>"
-    return HTML(html)
-    
-def exemple_comptage ():
-    return show_comptage (*comptage_inputs[0], comptage=comptage, suffix=".ok")
+    # on prend le premier jeu d'entrÃ©e
+    def exemple (self):
+        return show_comptage (*self.inputs[0], comptage=comptage, suffix=".ok")
 
-def debug_comptage (student_comptage):
-    return show_comptage (*comptage_inputs[0], comptage=student_comptage, suffix="")
+    def debug (self, student_comptage):
+        return show_comptage (*self.inputs[0], comptage=student_comptage, suffix="")
+
+
+exo_comptage = ExerciceComptage (comptage, comptage_inputs)
