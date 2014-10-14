006c541d1ea219fd1bd6591c3fbe4b289a3639f4
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-10-14 13:03:21 +0200

    le complement sur globals / locals

---
+++ modules/locals.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+un module pour illustrer la fonction locals
+"""
+
+# pour afficher
+from locals_globals import environnement
+
+def temoin (x):
+    "la fonction témoin"
+    y = x ** 2
+    print 20*'-','locals - entrée:'
+    environnement(locals())
+
+    for i in [1]:
+        for j in [1]:
+            print 20*'-','locals - boucle:'
+            environnement(locals())
+            
