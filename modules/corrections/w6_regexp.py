df1ee4fbc1f45b847bcf8e940e0fda15c331c745
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-11-24 14:11:00 +0100

    se en page corrige w6

--- modules/corrections/w6_regexp.py
+++ modules/corrections/w6_regexp.py
@@ -11,8 +11,9 @@ for germ in germs:
 
 # @BEG@ 6 6 regexp_pythonid
 # un identificateur commence par une lettre ou un underscore
-# et peut être suivi par n'importe quel nombre de lettre, chiffre
-# ou underscore, ce qui se trouve être \w si on ne se met pas en mode unicode
+# et peut être suivi par n'importe quel nombre de
+# lettre, chiffre ou underscore, ce qui se trouve être \w
+# si on ne se met pas en mode unicode
 regexp_pythonid = "[a-zA-Z_]\w*"
 # @END@
 
