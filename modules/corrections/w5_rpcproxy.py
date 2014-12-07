c9a044dd5f2c0a7eda73dae169ea026f76536bd3
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-12-07 16:43:13 +0100

    added docstrings in corrections for RCPProxy

--- modules/corrections/w5_rpcproxy.py
+++ modules/corrections/w5_rpcproxy.py
@@ -2,28 +2,43 @@
 # une troisième implémentation de RPCProxy
 
 class Forwarder(object):
-    def __init__(self, rpc_proxy, function):
-        self.function = function
+    """
+    Une instance de la classe Forwarder est un callable
+    qui peut être utilisée comme une méthode sur l
+    class RPCProxy
+    """
+    def __init__(self, rpc_proxy, methodname):
+        """
+        le constructeur  mémorise l'instance de RPCProxy
+        et le nom de la méthode qui a été appelée
+        """
+        self.methodname = methodname
         self.rpc_proxy = rpc_proxy
-    # en rendant cet objet callable, on peut l'utiliser
-    # comme méthode dans RPCProxy
+
     def __call__(self, *args):
+        """
+        en rendant cet objet callable, on peut l'utiliser
+        comme une méthode de RPCProxy
+        """
         print "Envoi à {}\nde la fonction {} -- args= {}".\
-            format(self.rpc_proxy.url, self.function, args)
-        return "retour de la fonction " + self.function
+            format(self.rpc_proxy.url, self.methodname, args)
+        return "retour de la fonction " + self.methodname
 
 class RPCProxy(object):
-    
+    """
+    Une troisième implémentation de RPCProxy qui sous-traite
+    à une classe annexe `Forwarder` qui se comporte comme
+    une *factory* de méthodes
+    """
     def __init__(self, url, login, password):
         self.url = url
         self.login = login
         self.password = password
         
-    def __getattr__ (self, function):
+    def __getattr__ (self, methodname):
         """
         Crée à la volée une instance de Forwarder
-        correspondant à 'function'
+        correspondant à 'methodname'
         """
-        return Forwarder(self, function)
-
+        return Forwarder(self, methodname)
 # @END@
