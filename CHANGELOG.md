1202423fbe0bd764cac3964db867d4209dc1ad3d
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-03-03 14:41:10 +0100

    for publishing on pypi

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# 0.1.0 - 2017 Feb 3
+* first versioned release
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+from nbautoeval.version import version
+
+def contents(localfile):
+    import os.path
+    full_path = os.path.join(os.path.dirname(__file__), localfile)
+    with open(full_path) as f:
+        return f.read()
+
+setup(
+    name             = "nbautoeval",
+    version          = version,
+    author           = "Thierry Parmentelat",
+    author_email     = "thierry.parmentelat@inria.fr",
+    description      = "A mini framework to implementauto-evaluated exercises in notebooks",
+    long_description = contents("README.md"),
+    license          = "CC BY-SA 4.0",
+    keywords         = "jupyter notebooks exercises",
+    url              = "https://github.com/parmentelat/nbautoeval",
+    packages         = [ 'nbautoeval' ],
+    classifiers      = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Programming Language :: Python",
+        # pypi won't let then in
+        # "Framework :: Jupyter",
+    ],
+)
