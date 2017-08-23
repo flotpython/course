ea22ebbaba5839fce0b143925eb3c726b3e70bbc
Arnaud Legout <arnaud.legout@inria.fr>
2017-08-23 10:08:49 +0200

    w4-s6 done

---
+++ w4/w4-s6-av-exo1.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+## Nous avons déjà vu la déclaration d'arguments ordonnées
+
+def agenda(nom, prenom, tel):
+    return {'nom' : nom , 'prenom' : prenom,
+            'tel' : tel}
+
+## et l'appel de fonction avec le passage d'arguments
+## ordonnés.
+
+d = agenda('idle', 'eric', '0720202020')
+print(d)
+
+## Il y a cependant une limitation avec cet appel : on doit connaître
+## l'ordre des arguments. Lorsqu'il y a beaucoup d'arguments et que le
+## nom des arguments est bien choisi il est plus facile de se souvenir
+## du nom des arguments que de leur ordre. C'est pourquoi, il est
+## possible d'appeler une fonction en plaçant les arguemnts dans un
+## ordre quelconque du moments qu'on les nomme
+
+d = agenda(tel='0720202020', nom='idle', prenom='eric')
+
+## Même s'il est possible de mélanger des arguments ordonnés et des
+## arguments nommés lors de l'appel, c'est rarement pertinent de le
+## faire. En effet, soit on connait l'ordre des arguments et dans ce
+## cas on utilise des arguments ordonnés lors de l'appel. Soit on ne
+## connait pas l'ordre alors on utilise des arguments nommés. Si on
+## les mélanges cela peut conduire à des erreurs subtiles que nous
+## détaillerons dans les compléments.
+
+## Si l'on revient à notre déclaration de fonction, on pourrait
+## vouloir créer une entrée dans l'agenda sans donner un
+## téléphone. C'est ce que l'on appelle un argument optionnel. Le
+## moyen le plus simple est d'utiliser les arguments par
+## défaut. Regardons un exemple
+
+def agenda(nom, prenom, tel='na'):
+    return {'nom' : nom , 'prenom' : prenom,
+            'tel' : tel}
+
+## si on ne founit pas l'argument lors de l'appel, c'est
+## l'argument par défaut qui est utilisé.
+print(agenda('idle', 'eric'))
+
+##sinon c'est l'argument que l'on passe. 
+print(agenda('idle', 'eric', '0720202020'))
+
+## lorsque dans la déclaration de notre fonction on a des
+## arguments ordonnés et des arguments par défaut,
+## il faut toujours mettre les arguments ordonnés en premier
+## et les arguments par défaut après. On ne peut pas les
+## mélanger. 
+
+## Nous avons donc vu deux manières de déclarer les arguments d'une
+## fonction : arguments ordonnés et arguments par défaut; et deux
+## manières d'appeler une fonction, arguments ordonnés et arguments
+## nommés.
+
+
---
+++ w4/w4-s6-av-exo2.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+## Regardons les deux dernières manières de déclarer des arguments et
+## de passer des arguments à une fonction.
+
+
+## regardons maintenant la forme *. C'est un nom étrange pour quelque
+## chose de très simple. Regardons un exemple
+
+def f(*t):
+    print t
+
+## le nom "t" est juste un nom de variable, on peut prendre n'importe
+## quel nom seul l'étoile est importante. Avec cette maniere de
+## déclarer les arguments, on peut passer une liste quelconque qui
+## sera mise dans un tuple référencé par la variable "t".
+
+f(1, 2, 3, 'a')
+
+## il nous reste une dernière forme à voir, la forme **.  Ici encore
+## c'est un nom étrange pour quelque chose de simple, regardons alors
+## un exemple...
+
+def f(**d):
+    print d
+
+## Ici aussi, "d" est juste un nom de variable, seule la double * est
+## importante. Avec cette déclaration d'arguments, on pourra appeler
+## notre fonction avec des arguments nommés et ces arguments seront
+## mis dans un dictionnaire avec pour clef le nom de l'argument et
+## pour valeur l'argument passé.
+
+f(nom='idle', prenom='eric', tel='0720202020')
+
+## On peut combiner les 4 déclarations d'arguments, mais toujours dans
+## l'ordre suivant arguments ordonnés, arguments par défaut, forme *,
+## forme **. Cependant, il ne faut pas que ça nuise à la clareté,
+## c'est pourquoi on recommande d'éviter de mélanger plus de deux
+## manières.
+
+## Vous pouvez vous demander dans quel cas on souhaite avoir une
+## fonction qui accepte un nombre quelconque d'arguments. Un exemple
+## d'utilisation de ces déclarations est la fonction built-in print
+
+print?
+
+## print accepte un nombre quelconque d'argument à afficher, on a donc
+## une forme *t, et quelques arguments par défauts : sep, end, file et
+## flush
+
+
+## Pour finir, il nous reste encore à voir deux manière
+## d'appeler une fonction. Regardons un exemple
+
+def f(a, b):
+    print(a, b)
+
+## Ma fonction prend deux arguments, mais supposons
+## que j'ai mes arguments à passer à la fonction dans une liste
+x = [1, 2]
+
+## je peux passer x[0] comme premier argument et x[1] comme deuxième,
+
+f(x[0], x[1])
+
+## mais je peux aussi utiliser une forme * qui permet de passer les
+## éléments d'une séquence comme arguments d'une fonction
+
+f(*x)
+
+## Supposons, maintenant que j'ai mes arguments dans un
+## dictionnaire
+d = {'a' : 1, 'b' : 2}
+
+## je peux utiliser ce dictionnaire pour passer des arguments
+## nommés à ma fonction avec une forme **, il faut évidement
+## que chaque clef du dictionnaire corresponde au nom d'un
+## argument de la fonction
+
+f(**d)
+
+## Cette forme peut vous paraître artificielle, mais elle a plusieurs
+## usages très important. Le premier est lorsque l'on veut appeler une
+## fonction avec toujours les mêmes arguments nommés, sans avoir à les
+## retaper à chaque fois. Regardons un exemple,
+
+pp = {'sep':'_', 'end':'.', 'flush':True} ## pp pour pretty print
+phrase = ['un', 'mooc', 'sur', 'python']
+
+print(*phrase, **pp)
+
+
+## Il y a un autre usage très important des formes * et **, c'est
+## l'implémentation de wrappers. Nous verrons ce sujet en profondeur
+## lorsque nous parlerons des décorateurs, mais je peux déjà vous
+## donner une idée de cela. Imaginez que je veuille créer une fonction
+## qui s'appelle comme la fonction print, mais qui affiche une ligne
+## de dièse, puis appelle print, puis affiche une ligne de dièse. La
+## difficulté ici est d'écrire une fonction qui a la même signature
+## que print. La bonne solution est d'écrire un wrapper qui prend
+## comme argument une forme * et **. Comme la forme * accepte
+## n'importe quel argument ordonnés et que la forme ** accepte
+## n'importes quels arguments nommés, notre wrapper va accepter
+## n'importe quelle liste d'argument ordonnés et nommés, donc
+## n'importe quelle signature.
+
+
+def my_print(*t, **d):
+    print('#'*30)
+    print(*t, **d)
+    print('#'*30)
+
+
+my_print(1, 2, 3, sep='_')
+ 
