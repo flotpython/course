3eca83c3b098a99d31d2b2ed620c4cabc4f11a94
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-09-11 16:12:33 +0200

    checkpoint with w9-s6 OK

--- modules/asynchelpers.py
+++ modules/asynchelpers.py
@@ -3,10 +3,10 @@ from datetime import datetime
 import asyncio
 
 # low-level API 
-def _start_time():
+def _start_timer():
     return datetime.now()
 
-def _show_time(start, *args):
+def _show_timer(start, *args):
     delta = datetime.now() - start
     duration = f"{delta.seconds}s + {delta.microseconds//1000:03d}ms"
     print(duration, *args)
@@ -14,19 +14,25 @@ def _show_time(start, *args):
 ####################
 # Use a module global to keep things simple
 
-start = None
+glo_start = None
 
-def start_time():
-    global start
+def start_timer():
+    global glo_start
     print("---------- zero")
-    start = _start_time()
+    glo_start = _start_timer()
 
-def show_time(*args):
-    _show_time(start, *args)
+def show_timer(*args):
+    global glo_start
+    _show_timer(glo_start, *args)
 
 ##############################
 async def sequence(*messages, delay=1):
-    show_time(">>>", *messages)
+    show_timer(">>>", *messages)
     await asyncio.sleep(delay)
-    show_time("<<<", *messages)
+    show_timer("<<<", *messages)
     
+##########
+def reset_loop():
+    asyncio.set_event_loop(asyncio.new_event_loop())
+
+
