9881dde05da695f092f35d9801ad2da8925c21c8
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-09-14 12:46:15 +0200

    checkpoint w8-s7

--- w8/calls7.py
+++ w8/calls7.py
@@ -3,9 +3,10 @@ import asyncio
 async def coro():
     print("dans coro")
 
-async def main():
-    # pareil mais depuis une coroutine
+async def amain():
+    # pareil mais depuis
+    # une coroutine
     coro()
 
 asyncio.get_event_loop().\
-   run_until_complete(main())
+   run_until_complete(amain())
