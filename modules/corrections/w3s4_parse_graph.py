bf62c7b84e77277680e9b1102da6a0b196a3da08
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-10-16 16:49:03 +0200

    exercice sur le parsing de graphes

---
+++ modules/corrections/w3s4_parse_graph.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from nbautoeval.exercise_function import ExerciseFunction
+from nbautoeval.args import Args
+
+#################### index    
+# @BEG@ name=index no_example=skip
+
+from collections import defaultdict
+
+def parse_graph(filename):
+    g = defaultdict(list)
+    with open(filename) as f:
+        for line in f:
+            begin, value, end = line.split()
+            g[begin].append( (end, int(value)))
+    return g
+# @END@
+
+inputs_parse_graph = [
+    Args("data/graph1.txt"),
+    Args("data/graph2.txt"),
+    Args("data/graph3.txt"),
+]
+
+exo_parse_graph = ExerciseFunction(parse_graph,
+                                   inputs_parse_graph,
+                                   nb_examples = 1,
+)
+
+
+
