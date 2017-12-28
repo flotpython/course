5c4d39114fc746b94dff42fe419030eb583cc82e
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2017-12-28 20:04:14 +0100

    module broadcasting un peu mieux commenté

--- modules/broadcasting.py
+++ modules/broadcasting.py
@@ -1,8 +1,21 @@
 from operator import mul
 from functools import reduce
+from itertools import zip_longest
 
 import numpy as np
 
+"""
+Une fonction `compatible` (en deux versions)
+qui permet de vérifier si deux dimensions 
+de tableaux numpy sont ou non compatibles 
+pour le broadcasting
+"""
+
+
+#################### 
+# v1: la version qui triche
+# on fait faire le travail à numpy et on
+# regarde comment ça se passe
 def compatible(t1, t2):
     """
     Une fonction qui indique si deux tableaux numpy 
@@ -22,9 +35,16 @@ def compatible(t1, t2):
         return result.shape
     except ValueError as e:
         return False
-            
-# comparer deux longueurs dans une dimension donnée
+
+
+####################
+# v2:
+# on calcule nous-mêmes si deux tuples
+# sont compatibles ou non
+
+
 def dmax(d1, d2):
+    "compare deux longueurs dans une dimension donnée"
     # same lengths
     if d1 == d2:
         return d1
@@ -34,6 +54,7 @@ def dmax(d1, d2):
     # at least one is 1
     else:
         return max(d1, d2)
+
 
 def compatible_bool(t1, t2):
     """
@@ -47,7 +68,6 @@ def compatible_bool(t1, t2):
         dmax(d1, d2)
         for d1, d2 in zip(reversed(t1), reversed(t2)))
 
-from itertools import zip_longest
 
 def compatible2(t1, t2):
     """
@@ -57,7 +77,7 @@ def compatible2(t1, t2):
     rev_dmaxes = [
         dmax(d1, d2)
         for (d1, d2) in zip_longest(
-                reversed(t1), reversed(t2), fillvalue=1)]
+            reversed(t1), reversed(t2), fillvalue=1)]
     if not all(rev_dmaxes):
         return False
     rev_dmaxes.reverse()
