9b72556a67ed17d20431dd0bc73abd377861282e
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2018-07-09 11:44:08 +0200

    comments on w7-s7

--- w7/w7-s7-av-exo1.py
+++ w7/w7-s7-av-exo1.py
@@ -1,5 +1,9 @@
 ##### Initialiser le notebook avec le code ci-dessous
 ##### BEGIN
+
+import numpy as np
+import pandas as pd
+
 # liste des noms
 prenoms = ['liz', 'bob', 'bill', 'eve']
 
@@ -11,11 +15,15 @@ sexe = pd.Series(list('fhhf'), index=prenoms)
 # création de la DataFrame
 df = pd.DataFrame({'age': age, 'taille': taille, 'sexe': sexe})
 ##### END
- 
+
 # Regardons ce code pour créer une DataFrame. N'hésitez
 # pas à mettre la vidéo en pause pour avoir le temps de taper
-# ce code. 
+# ce code.
 print(df)
+
+[thierry] puisque tu es dans un notebook tu devrais faire juste:
+df
+sans le print(), le resultat est plus joli..
 
 #1m00
 
@@ -40,7 +48,7 @@ df.describe()
 
 # 2m11
 
-# Regardons maintenant l'indexation. 
+# Regardons maintenant l'indexation.
 # Comme pour les Series, je vous recommande de toujours
 # utiliser les attributs loc et iloc. Loc est pour l'indexation sur les
 # label et iloc sur les rangs des labels dans les index
@@ -50,16 +58,18 @@ df.describe()
 
 df.loc['liz']
 
-# si vous donner deux arguments, le premier sera pour 
+# si vous donner deux arguments, le premier sera pour
 # l'index des lignes et le deuxième pour l'index des colonnes
 df.loc['liz', 'age']
 
 # On peut faire du slicing sur les lignes et le colonnes
 df.loc[:, 'taille']
 
-# on peut évidemment faire de l'indexation avancée 
+# on peut évidemment faire de l'indexation avancée
 
 df.loc[p.loc[:,'age']< 32]
+      ^^^
+attention c'est df et pas p
 
 # Il est courant avec une DataFrame de transformer un index
 # en colonne et une colonne en index. Regardons cela
@@ -77,7 +87,7 @@ print(df)
  # Les méthodes sur les DataFrame retournent en général une
  # nouvelle DataFrame, il est par conséquent courant chainer
  # les appelles de méthodes de la manière suivante
- 
+
 noms = ['liz', 'bob', 'bill', 'eve']
 age = pd.Series([25, 30, 35, 40], index=noms)
 taille = pd.Series([160, 175, 170, 180], index=noms)
@@ -87,36 +97,45 @@ df = pd.DataFrame({'age': age, 'taille': taille, 'sexe': sexe})
 df = (df.reset_index()
     .rename(columns={'index':'nom'})
     .set_index('age'))
-	
+
 #6m30
 
-# regardons maintenant un exemple sur l'alignement des 
+# regardons maintenant un exemple sur l'alignement des
 # index lors des opérations entre DataFrames
 
 df1 = pd.DataFrame(np.ones((2,2)), index=list('ab'), columns=list('xy'))
 df2 = pd.DataFrame(np.ones((2,2)), index=list('ac'), columns=list('xz'))
 
+ce sera plus clair si tu montres d'abord df1 et df2:
+
+df1
+df2
+
 df1+df2
 
-# Si j'aditionne les deux DataFrames, les valeurs manquantes 
+# Si j'aditionne les deux DataFrames, les valeurs manquantes
 # sont automatiquement replacée par NaN. Je peux controller
 # ce comportement en appelant la méthodes correspondant
 # à l'opération avec l'argument fill_value
 
 df = df1.add(df2, fill_value=0)
-print(df)
+df
 # Par contre, si des valeurs sont manquantes dans les deux DataFrame
-# le résultat sera tout de même NaN. Je peux cependant, 
+# le résultat sera tout de même NaN. Je peux cependant,
 # remplacer les NaN et effacer les lignes les contenants.
 
 df.fillna(-1) # pour remplacer les NaN par un -1
+df
 
 df.dropna() # pour supprimer les lignes contenant un NaN
+df
 
 # Vous verrez dans les compléments que ces methodes ont
 # plusieurs options qui par exemple de remplacer les NaN
-# avec une valeurs proches du NaN dans le tableau ou 
+# avec une valeurs proches du NaN dans le tableau ou
 # de supprimer une contenant uniquement un certains
-# nombre de NaN. 
+# nombre de NaN.
+J'insiste lourdement mais on est bien d'accord que je te laisserai
+faire tous ces compléments que tu annonces dans les vidéos :)
 
 #9m30
