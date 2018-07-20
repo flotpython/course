49f85f4799ec8e3c47d40e07d82afe8a9508fce8
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2018-07-20 09:21:40 +0200

    exo wc: shorter inputs, and renumbered as w2s6x5

--- modules/corrections/exo_wc.py
+++ modules/corrections/exo_wc.py
@@ -17,15 +17,16 @@ def wc(string):
     return [nb_lines, nb_words, nb_bytes]
 # @END@
 
-wc_input = (
+wc_inputs = (
     Args('''Python is a programming language
 that lets you work quickly
 and integrate systems more effectively.'''),
     Args(''),
     Args('abc'),
     Args('abc \t'),
+    Args('a  bc \t'),
     Args(' \tabc \n'),
-    Args('a b c d e f g h i j k l m n o p q r s t u v w x y z\n'),
+    Args(" ".join("abcdefg") + "\n"),
     Args('''The Zen of Python, by Tim Peters
 
 Beautiful is better than ugly.
@@ -34,20 +35,7 @@ Simple is better than complex.
 Complex is better than complicated.
 Flat is better than nested.
 Sparse is better than dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
-Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
-'''),
+...'''),
     )
 
-exo_wc = ExerciseFunction(wc, wc_input, layout_args=(80, 15, 15))
+exo_wc = ExerciseFunction(wc, wc_inputs, layout_args=(80, 15, 15))
