5c62d0307ed860bf9e9f471d90709cce2196c52d
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2018-10-03 21:49:18 +0200

    avoid using input as a variable name; use in_file and out_file instead

--- modules/corrections/exo_comptage.py
+++ modules/corrections/exo_comptage.py
@@ -14,8 +14,8 @@ import os
 # so here create helpers to make all this compliant
 def file_contents(filename):
     try:
-        with open(filename, encoding='utf-8') as input:
-            return input.read()
+        with open(filename, encoding='utf-8') as in_file:
+            return in_file.read()
     except:
         return ""
 
@@ -44,7 +44,7 @@ def show_comptage(in_name, out_name, comptage, suffix):
     out_name += suffix
     comptage(in_name, out_name)
     try:
-        input, output = file_contents(in_name), file_contents(out_name)
+        in_file, out_file = file_contents(in_name), file_contents(out_name)
         os.unlink(out_name)
     except OSError:
         print("Votre fonction ne semble pas créer le fichier de sortie")
@@ -52,10 +52,10 @@ def show_comptage(in_name, out_name, comptage, suffix):
     html = ""
     html += "<table>"
     html += "<tr><th>Entrée</th></tr>"
-    for line in input.split("\n"):
+    for line in in_file.split("\n"):
         html += "<tr><td>{}</td></tr>".format(line)
     html += "<tr><th>Sortie</th></tr>"
-    for line in output.split("\n"):
+    for line in out_file.split("\n"):
         html += "<tr><td>{}</td></tr>".format(line)
     html += "</table>"
     return HTML(html)
@@ -69,20 +69,20 @@ def comptage(in_filename, out_filename):
     et de caractères
     """
     # on ouvre le fichier d'entrée en lecture
-    with open(in_filename, encoding='utf-8') as input:
+    with open(in_filename, encoding='utf-8') as in_file:
         # on ouvre la sortie en écriture
-        with open(out_filename, 'w', encoding='utf-8') as output:
+        with open(out_filename, 'w', encoding='utf-8') as out_file:
             lineno = 1
             # pour toutes les lignes du fichier d'entrée
             # le numéro de ligne commence à 1
-            for line in input:
+            for line in in_file:
                 # autant de mots que d'éléments dans split()
                 nb_words = len(line.split())
                 # autant de caractères que d'éléments dans la ligne
                 nb_chars = len(line)
                 # on écrit la ligne de sortie; pas besoin
                 # de newline (\n) car line en a déjà un
-                output.write(f"{lineno}:{nb_words}:{nb_chars}:{line}")
+                out_file.write(f"{lineno}:{nb_words}:{nb_chars}:{line}")
                 lineno += 1
 # @END@
 
@@ -92,16 +92,16 @@ def comptage_bis(in_filename, out_filename):
     """
     un peu plus pythonique avec enumerate
     """
-    with open(in_filename, encoding='utf-8') as input:
-        with open(out_filename, 'w', encoding='utf-8') as output:
+    with open(in_filename, encoding='utf-8') as in_file:
+        with open(out_filename, 'w', encoding='utf-8') as out_file:
             # enumerate(.., 1) pour commencer avec une ligne
             # numérotée 1 et pas 0
-            for lineno, line in enumerate(input, 1):
+            for lineno, line in enumerate(in_file, 1):
                 # une astuce : si on met deux chaines
                 # collées comme ceci elle sont concaténées
                 # et on n'a pas besoin de mettre de backslash
                 # puisqu'on est dans des parenthèses
-                output.write(f"{lineno}:{len(line.split())}:"
+                out_file.write(f"{lineno}:{len(line.split())}:"
                              f"{len(line)}:{line}")
 # @END@
 
@@ -111,20 +111,20 @@ def comptage_ter(in_filename, out_filename):
     """
     pareil mais avec un seul with
     """
-    with open(in_filename, encoding='utf-8') as input, \
-         open(out_filename, 'w', encoding='utf-8') as output:
-        for lineno, line in enumerate(input, 1):
-            output.write(f"{lineno}:{len(line.split())}:"
+    with open(in_filename, encoding='utf-8') as in_file, \
+         open(out_filename, 'w', encoding='utf-8') as out_file:
+        for lineno, line in enumerate(in_file, 1):
+            out_file.write(f"{lineno}:{len(line.split())}:"
                          f"{len(line)}:{line}")
 # @END@
 
 
 
 def comptage_ko(in_filename, out_filename):
-    with open(in_filename) as input:
-        with open(out_filename, 'w') as output:
-            for lineno, line in enumerate(input):
-                output.write(f"{lineno}:{len(line.split())}:"
+    with open(in_filename) as in_file:
+        with open(out_filename, 'w') as out_file:
+            for lineno, line in enumerate(in_file):
+                out_file.write(f"{lineno}:{len(line.split())}:"
                              f"{len(line)}:{line}")
 
 # on passe ceci à ExerciseFunction donc pas besoin de rajouter les **keywords
@@ -146,12 +146,12 @@ class ExoComptage(ExerciseFunction):
     # et là-dedans il nous faut regarder dans .args qui va contenir
     # un tuple avec les deux valeurs qu'on recherche
     def example(self):
-        (input, output) = self.datasets[0].args
-        return show_comptage(input, output, comptage=comptage, suffix=".ok")
+        (in_file, out_file) = self.datasets[0].args
+        return show_comptage(in_file, out_file, comptage=comptage, suffix=".ok")
 
     def debug(self, student_comptage):
-        (input, output) = self.datasets[0].args
-        return show_comptage(input, output, comptage=student_comptage, suffix="")
+        (in_file, out_file) = self.datasets[0].args
+        return show_comptage(in_file, out_file, comptage=student_comptage, suffix="")
 
 
 exo_comptage = ExoComptage(
