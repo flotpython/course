883e6398057adee1edb6f749adc99487a147d658
parmentelat <thierry.parmentelat@inria.fr>
2018-11-08 11:14:26 +0100

    reviewed exo stairs and moved it one step later, making space for exo checkers

--- modules/corrections/exo_stairs.py
+++ modules/corrections/exo_stairs.py
@@ -1,41 +1,46 @@
+# pylint: disable=c0111
+
 import numpy as np
+
+
 
 from nbautoeval.exercise_function import ExerciseFunctionNumpy
 from nbautoeval.args import Args
 
 
 # @BEG@ name=stairs
-def stairs(k):
+def stairs(taille):
     """
     la pyramide en escaliers telle que décrite dans l'énoncé
     """
     # on calcule n
-    n = 2 * k + 1
+    n = 2 * taille + 1
     # on calcule les deux tableaux d'indices
     # tous les deux de dimension n
     ix, iy = np.indices((n, n))
     # il n'y a plus qu'à appliquer la formule qui va bien
-    return 2 * k - (np.abs(ix - k) + np.abs(iy - k))
+    return 2 * taille - (np.abs(ix - taille) + np.abs(iy - taille))
 # @END@
 
 
 # @BEG@ name=stairs more=bis
-def stairs_bis(k):
+def stairs_bis(taille):
     """
     Bien sûr on peut préciser le type mais ce n'est pas
     réellement nécessaire ici
     """
-    n = 2 * k + 1
+    n = 2 * taille + 1
     ix, iy = np.indices((n, n), dtype=np.int8)
-    return 2 * k - (np.abs(ix - k) + np.abs(iy - k))
+    return 2 * taille - (np.abs(ix - taille) + np.abs(iy - taille))
 # @END@
 
 
 
-def stairs_ko(k):
-    n = 2 * k + 1
+def stairs_ko(taille):
+    n = 2 * taille + 1
     ix, iy = np.indices((n, n), dtype=np.float)
-    return (k - 1) + 2 * k - (np.abs(ix - k) + np.abs(iy - k))
+    return ((taille - 1) + 2 * taille
+            - (np.abs(ix - taille) + np.abs(iy - taille)))
 
 
 stairs_inputs = [
@@ -49,5 +54,5 @@ stairs_inputs = [
 exo_stairs = ExerciseFunctionNumpy(
     stairs,
     stairs_inputs,
-    nb_examples = 2,
+    nb_examples=2,
     )
