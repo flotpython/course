ccbad560ef963e1ae96e866280851c9e2f353723
parmentelat <thierry.parmentelat@inria.fr>
2018-11-20 13:18:15 +0100

    clarifer exo dice

--- modules/corrections/exo_dice.py
+++ modules/corrections/exo_dice.py
@@ -34,27 +34,65 @@ def dice(target, nb_dice=2, sides=6):
 
 
 # @BEG@ name=dice more=bis
-# la clé c'est d'utiliser itertools.product
-# qui nous permet d'itérer sans aucune mémoire
-# sur le même hypercube
+# on peut aussi utiliser itertools.product qui permet
+# d'itérer sans aucune mémoire sur le même hypercube
+#
+# de manière un peu paradoxale, ces versions en python pur,
+# bien que nécessitant en théorie beaucoup moins de mémoire,
+# sont beaucoup moins efficaces que la version numpy
+# je vous renvoie à la discussion sur le forum intitulée
+# "Exercice dice"
 from itertools import product
 
 def dice_bis(target, nb_dice=2, sides=6):
     """
-    Une autre méthode complètement, qui fonctionne sans numpy
+    Une autre méthode complètement, qui n'alloue aucun tableau
+    du coup on n'a pas besoin de numpy
     """
-    # par exemple le cas standard se ferait avec
-    # for (i, j) in itertools.product(range(1, 7), range(1, 7)):
-    #    blabla
-    matches = 0
+    # en version facile, on peut utiliser le paramètre `repeat`
+    # de product qui fait exactement ce qu'on veut, puisque
+    # tous les dés ont le même nombre de faces
+    #
+    # par exemple le cas standard (2 dés, 6 faces) se ferait avec
+    # quelque chose comme
+    # (for (i, j) in itertools.product(range(1, 7), repeat=2))
+    #
+    # le premier sum compte les occurences de True dans l'itération
+    return sum(
+        # ici sum(x) fait la somme des tirages des dés
+        sum(x) == target
+        for x in product(range(1, sides+1), repeat=nb_dice))
+# @END@
+
+
+# @BEG@ name=dice more=ter
+from itertools import product
+
+def dice_ter(target, nb_dice=2, sides=6):
+    """
+    La même chose, mais on voit une méthode qui n'utilise pas
+    repeat dans itertools.product
+    Ça pourrait être utile si par exemple les dés n'avaient
+    pas tous la même taille
+    """
     # on construit les <nb_dice> instances de range() qui serviront
     # de base à itertools
     iterators = (range(1, sides+1) for dimension in range(nb_dice))
+    return sum(sum(x) == target for x in product(*iterators))
+# @END@
+
+
+# @BEG@ name=dice more=quat
+def dice_quat(target, nb_dice=2, sides=6):
+    """
+    Cette version semble un peu plus rapide
+    """
+    iterators = (range(1, sides+1) for dimension in range(nb_dice))
+    count = 0
     for sample in product(*iterators):
-        # sample ici est un tuple
         if sum(sample) == target:
-            matches += 1
-    return matches
+            count += 1
+    return count
 # @END@
 
 
