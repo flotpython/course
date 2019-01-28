53191b5540b0904758b907c7c5b370c673d8aaee
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-01-28 15:40:23 +0100

    Fibonacci commence avec 0->0,1->1 et non plus 0->1,1->1

--- data/fibonacci.py
+++ data/fibonacci.py
@@ -1,21 +1,35 @@
-## La suite de Fibonacci (Suite)
+# La suite de Fibonacci (version pour terminal)
 
 from argparse import ArgumentParser
 
-def fibonacci (n):
+def fibonacci(n):
     "retourne le nombre de fibonacci pour l'entier n"
-    if n <= 1 : 
-        return 1
-    f2, f1 = 1, 1
-    for i in range (2,n+1):
-        f2, f1 = f1, f1+f2
-#        print i, f2, f1
+    # pour les deux premières valeurs de n, on peut renvoyer n
+    if n <= 1:
+        return n
+    # sinon on initialise f1 pour n-1 et f2 pour n-2
+    f2, f1 = 0, 1
+    # et on itère n-1 fois pour additionner
+    for i in range(2, n + 1):
+        f2, f1 = f1, f1 + f2
+#        print(i, f2, f1)
+    # le résultat est dans f1
     return f1
 
-parser = ArgumentParser ()
-parser.add_argument (dest="entier", type=int, 
-                     help="entier d'entree")
+# à nouveau : ceci n'est pas conçu pour être exécuté dans le notebook !
+parser = ArgumentParser()
+parser.add_argument(dest="entier", type=int,
+                    help="entier d'entrée")
 input_args = parser.parse_args()
 entier = input_args.entier
-print("fibonacci({}) = {}".format(entier, fibonacci (entier)))
 
+# Nous pouvons à présent afficher le résultat :
+print(f"fibonacci({entier}) = {fibonacci(entier)}")
+
+
+# Vous pouvez donc à présent exécuter ce script
+# avec simplement l'interpréteur Python comme ceci :
+# 
+# $ python3 fibonacci.py 56
+# 
+# (et sans taper le signe $ qui indique simplement le prompt du terminal)
--- w1/fibonacci_prompt.py
+++ w1/fibonacci_prompt.py
@@ -1,7 +1,11 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
-# <span style="float:left;">Licence CC BY-NC-ND</span><span style="float:right;">Thierry Parmentelat &amp; Arnaud Legout&nbsp;<img src="media/both-logos-small-alpha.png" style="display:inline"></span><br/>
+# <style>div.title-slide {    width: 100%;    display: flex;    flex-direction: row;            /* default value; can be omitted */    flex-wrap: nowrap;              /* default value; can be omitted */    justify-content: space-between;}</style><div class="title-slide">
+# <span style="float:left;">Licence CC BY-NC-ND</span>
+# <span>Thierry Parmentelat &amp; Arnaud Legout</span>
+# <span><img src="media/both-logos-small-alpha.png" style="display:inline" /></span>
+# </div>
 
 # # La suite de Fibonacci
 
@@ -11,36 +15,37 @@
 # 
 # Nous allons commencer par le faire tourner dans ce notebook. Nous verrons en fin de séance comment le faire fonctionner localement sur votre ordinateur.
 
-# Le but de ce programme est de calculer la fonction de Fibonacci, qui comme vous vous en souvenez sûrement, est définie comme ceci:
+# Le but de ce programme est de calculer la [suite de Fibonacci](https://fr.wikipedia.org/wiki/Suite_de_Fibonacci), qui est définie comme ceci&nbsp;:
 # 
-# * $ u_0 = 1$
-# * $u_1 = 1 $
-# * $ \forall n >= 2, u_{n} = u_{n-1} + u_{n-2} $
+# * $u_0 = 0$
+# * $u_1 = 1$
+# * $\forall n >= 2, u_n = u_{n-1} + u_{n-2}$
 # 
-# Ce qui donne pour les premières valeurs
+# Ce qui donne pour les premières valeurs :
 
-# | n | fibonacci(n) |
-# |---|-----------|
-# | 0 | 1         |
-# | 1 | 1         |
-# | 2 | 2         |
-# | 3 | 3         |
-# | 4 | 5         |
-# | 5 | 8         |
-# | 6 | 13        |
+# | n  | fibonacci(n)  |
+# |---:|--------------:|
+# | 0  | 0             |
+# | 1  | 1             |
+# | 2  | 1             |
+# | 3  | 2             |
+# | 4  | 3             |
+# | 5  | 5             |
+# | 6  | 8             |
+# | 7  | 13            |
 
-# On commence par définir la fonction `fibonacci` comme suit. Naturellement vous n'avez pas encore tout le bagage pour lire ce code, ne vous inquiétez pas nous allons vous expliquer tout ça dans les prochaines semaines. Le but est uniquement de vous montrer un fonctionnement de l'interpréteur Python et de IDLE. 
+# On commence par définir la fonction `fibonacci` comme il suit. Naturellement vous n'avez pas encore tout le bagage pour lire ce code, ne vous inquiétez pas, nous allons vous expliquer tout ça dans les prochaines semaines. Le but est uniquement de vous montrer un fonctionnement de l'interpréteur Python et de IDLE.
 
-# In[3]:
+# In[ ]:
 
 
 def fibonacci(n):
     "retourne le nombre de fibonacci pour l'entier n"
-    # pour les petites valeurs de n il n'y a rien à calculer
-    if n <= 1: 
-        return 1
-    # sinon on initialise f1 pour n-1 et f2 pour n-2
-    f2, f1 = 1, 1
+    # pour les deux petites valeurs de n, on peut retourner n
+    if n <= 1:
+        return n
+    # sinon on initialise f2 pour n-2 et f1 pour n-1
+    f2, f1 = 0, 1
     # et on itère n-1 fois pour additionner
     for i in range(2, n + 1):
         f2, f1 = f1, f1 + f2
@@ -49,17 +54,17 @@ def fibonacci(n):
     return f1
 
 
-# Pour en faire un programme utilisable on va demander à l'utilisateur de rentrer un nombre; il faut le convertir en entier car `input` renvoie une chaîne de caractères
+# Pour en faire un programme utilisable on va demander à l'utilisateur de rentrer un nombre&nbsp;; il faut le convertir en entier car `input` renvoie une chaîne de caractères :
 
-# In[4]:
+# In[ ]:
 
 
 entier = int(input("Entrer un entier "))
 
 
-# On imprime le résultat
+# On imprime le résultat :
 
-# In[5]:
+# In[ ]:
 
 
 print(f"fibonacci({entier}) = {fibonacci(entier)}")
@@ -68,18 +73,20 @@ print(f"fibonacci({entier}) = {fibonacci(entier)}")
 # ### Exercice
 
 # Vous pouvez donc à présent&nbsp;:
-#  * exécuter le code dans ce notebook
-#  * télécharger ce code sur votre disque comme un fichier `fibonacci_prompt.py` 
-#    * utilisez pour cela le menu *"File -> Download as -> python"*
-#    * et renommez le fichier obtenu au besoin
-#  * l'exécuter sous IDLE
-#  * le modifier, par exemple pour afficher les résultats intermédiaires 
-#    * on a laissé exprès des commentaires que vous pouvez réactiver simplement
-#  * l'exécuter avec línterpréteur python comme ceci
-#  
-#      `$ python3 fibonacci_prompt.py`
-#      
+# 
+# * exécuter le code dans ce notebook
+# * télécharger ce code sur votre disque comme un fichier `fibonacci_prompt.py`
+#   * utiliser pour cela le menu *"File -> Download as -> Python"*
+#   * et **renommer le fichier obtenu** au besoin
+# * l'exécuter sous IDLE
+# * le modifier, par exemple pour afficher les résultats intermédiaires
+#   * on a laissé exprès une fonction `print` en commentaire que vous pouvez réactiver simplement
+# * l'exécuter avec l'interpréteur Python comme ceci :
+# 
+# ```bash
+# $ python3 fibonacci_prompt.py
+# ```
 
 # Ce code est volontairement simple et peu robuste pour ne pas l'alourdir. Par exemple, ce programme se comporte mal si vous entrez un entier négatif.
 
-# Nous allons voir tout de suite une version légèrement différente qui vous permet de spécifier la valeur d'entrée sur la ligne de commande.
+# Nous allons voir tout de suite une version légèrement différente qui va vous permettre de donner la valeur d'entrée sur la ligne de commande.
