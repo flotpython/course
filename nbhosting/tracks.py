9cf9adc108234d64f6c393659f7a841db6abaa59
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-03-09 16:59:41 +0100

    new names in tracks.py, forget Sections and use Track instead

--- nbhosting/tracks.py
+++ nbhosting/tracks.py
@@ -2,20 +2,21 @@
 This module is for nbhosting
 
 is describes the tracks available in the course,
-and for each of them provides a more legible
-sectioning than the one we could guess from the file system names
+and for each of them provides a more legible sectioning
+than the one we could guess from the file system names
 
 NOTE that notebook metadata like a human-friendly name can be provided
 in the 'notebookname' and 'version' metadata keys
 
 so as far as individual notebooks are concerned,
 it's enough to provide notebook paths;
-however it is desirable to provide also a human-friendly name for each section,
+however it is desirable to provide also a human-friendly
+name for each section,
 which cannot be found elsewhere on the disk.
 """
 from nbhosting.courses import (
-    Sections, Section, Notebook,
-    notebooks_by_pattern, sections_by_directory,
+    Track, Section, Notebook,
+    notebooks_by_pattern, track_by_directory,
     DEFAULT_TRACK)
 
 
@@ -31,27 +32,23 @@ WEEK_NAMES = {
     'w9': "9. Sujets avancés",
 }
 
+# we use this pattern in -x* to create a track
+# that contains only exercises
 def _exo_track(coursedir):
-    return sections_by_directory(
+    return track_by_directory(
         coursedir,
         notebooks_by_pattern(coursedir, "w?/w*-s*-x*.ipynb"),
-            dir_labels = WEEK_NAMES)
+        dir_labels=WEEK_NAMES)
 
 
-# mostly for testing the generic sectioning code
-def _generic_track(coursedir):
-    return sections_by_directory(
-        coursedir,
-        notebooks_by_pattern(coursedir, "w?/w*-s*-[cx]*.ipynb"))
-
-
+# plain track contains everything under w?
 def _course_track(coursedir):
     # a more elaborate sectioning, still based
     # on the filesystem structure but with better names
-    return sections_by_directory(
+    return track_by_directory(
         coursedir,
         notebooks_by_pattern(coursedir, f"w?/w*-s*-[cx]*.ipynb"),
-        dir_labels = WEEK_NAMES)
+        dir_labels=WEEK_NAMES)
 
 
 def tracks(coursedir):
@@ -60,18 +57,10 @@ def tracks(coursedir):
     at the root directory of the filesystem tree
     that holds notebooks
 
-    this returns a dictionary that maps track names to sectioning data
-
-    it can be either a single Sections object,
-    or if you want to define several tracks, a dictionary
-    'track_name' -> Sections obj
-
-    in the latter case, it is expected that the dictionary
-    contains a key named 'course'
+    this returns a dictionary that maps track names to Track instance
     """
 
     return {
         'exos': _exo_track(coursedir),
-#        'generic': _generic_track(coursedir),
         DEFAULT_TRACK: _course_track(coursedir),
     }
