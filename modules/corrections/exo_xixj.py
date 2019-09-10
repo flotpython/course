3b6f81103f67de165fea580b1ab240b415bdc1a9
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-09-10 12:09:06 +0200

    simplification de xxt en xixj

---
+++ modules/corrections/exo_xixj.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+from nbautoeval.exercise_function import ExerciseFunctionNumpy
+from nbautoeval.args import Args
+
+
+# @BEG@ name=xixj
+import numpy as np
+
+def xixj(*args):
+    """
+    si les arguments sont x1, x2, .. xn
+    retourne une matrice carrée n x n 
+    dont les éléments valent
+    m[i, j] = xi * xj
+    
+    dans cette première solution on va faire
+    le produit de matrices entre un vecteur colonne
+    et sa transposée
+    """
+    # par la déclaration '*args' 
+    # args désigne un tuple contenant x1, x2, ... xn
+    #
+    # on veut construire un vecteur colonne
+    # donc de forme (n, 1) à partir des arguments
+    # 
+    # pour bien décomposer on commence 
+    # par faire un vecteur ligne à partir de args
+    line = np.array(args)
+    # transformons en colonne
+    n = len(args)
+    column = line.reshape((n, 1))
+    # on obtient la transposée avec l'attribut T
+    # et pour la multiplication de matrices
+    # multiplier on utilise l'opérateur @
+    return column @ column.T
+# @END@
+
+
+# @BEG@ name=xixj more=bis
+def xixj_bis(*args):
+    """
+    pareil mais en utilisant .dot()
+    """
+    # on calcule un vecteur colonne comme ci-dessus
+    n = len(args)
+    column = np.array(args).reshape((n, 1))
+    # dans cette version on fait le produit de matrice
+    # en utilisant la méthode dot sur les tableaux
+    return column.dot(column.T)
+    # remarquez qu'on aurait pu faire aussi bien
+    # return np.dot(column, column.T)
+# @END@
+
+# @BEG@ name=xixj more=ter
+def xixj_ter(*args):
+    """
+    pareil mais en utilisant .. le broadcasting
+    car dans ce cas précis si on multiplie 
+    (au sens de la multiplication classique '*')
+    un vecteur colonne (n, 1) 
+    avec un vecteur ligne (1, n)
+    on fait du broadcasting et il se trouve
+    que c'est équivalent au produit matriciel
+    dans ce cas-là
+    """
+    n = len(args)
+    column = np.array(args).reshape((n, 1))
+    # le broadcasting c'est magique parfois
+    # car avec cette méthode on peut multiplier
+    # dans n'importe quel ordre !
+    # return column * column.T
+    return column.T * column
+# @END@
+
+def xixj_ko(*args):
+    # presque ça mais sans le reshape
+    return array.T @ array
+
+inputs_xixj = [
+    Args(1),
+    Args(1, 2),
+    Args(1, 2, 4),
+    Args(1, 0, 4),
+    Args(8, 4, 2),
+    Args(0, 1, 2, 4, 8),
+]
+
+exo_xixj = ExerciseFunctionNumpy(
+    xixj, inputs_xixj,
+    nb_examples = 3,
+)
