364e1c7ca09d32b25efcc4f62ebdb6ac9280097f
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-10-07 18:57:45 +0200

    refined solutions for two_sum

--- modules/corrections/exo_two_sum.py
+++ modules/corrections/exo_two_sum.py
@@ -4,9 +4,51 @@ from nbautoeval.args import Args
 
 # @BEG@ name=two_sum
 def two_sum(liste, target):
+    """
+    retourne un tuple de deux indices de deux nombres 
+    dans la liste dont la somme fait target
+    """
     for i, item1 in enumerate(liste):
         for j, item2 in enumerate(liste):
-            if j <= i:
+            # prune the loop on j altogether once we reach i
+            if j >= i:
+                break
+            if item1 + item2 == target:
+                return j, i
+# @END@
+
+
+# @BEG@ name=two_sum more=bis
+from itertools import product
+
+
+def two_sum_bis(liste, target):
+    """
+    pareil en utilisant itertools.product
+    pour éviter les deux for imbriqués
+    un tout petit peu moins efficace ici car on est dans une seule
+    boucle et donc on ne peut pas avorter la boucle interne
+    avec break
+    """
+    for (i, item1), (j, item2) in product(
+        enumerate(liste), enumerate(liste)):
+        for j, item2 in enumerate(liste):
+            if i >= j:
+                continue
+            if item1 + item2 == target:
+                return i, j
+# @END@
+
+
+# @BEG@ name=two_sum more=ter
+def two_sum_ter(liste, target):
+    """
+    toujours avec product, pour illustrer l'usage de repeat=
+    """
+    for (i, item1), (j, item2) in product(
+        enumerate(liste), repeat=2):
+        for j, item2 in enumerate(liste):
+            if i >= j:
                 continue
             if item1 + item2 == target:
                 return i, j
@@ -20,6 +62,7 @@ inputs = [
     Args([0, 64, 1, 128, 4, 8, 16, 32, 2], 3),
     Args([0, 64, 1, 127, 4, 8, 16, 32, 2], 128),
 ]
+
 
 exo_two_sum = ExerciseFunction(
     two_sum,
