a99480015f3824d29054a18bd479c504caf77807
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-04-23 10:01:49 +0200

    folded

--- data/broken-await.py
+++ data/broken-await.py
@@ -4,5 +4,6 @@ async def hello_world():
     await asyncio.sleep(0.2)
     print("Hello World")
 
-# ceci ne fonctionne pas en Python standard
+# ceci ne fonctionne pas
+# en Python standard
 await hello_world()
