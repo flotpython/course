9220439056a15b02782ad17928fd60890602e340
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-05-10 21:12:00 +0200

    fix broken yaml

--- w7/w7-s10-c6-fourier-k3d.py
+++ w7/w7-s10-c6-fourier-k3d.py
@@ -12,7 +12,7 @@
 #     display_name: Python 3
 #     language: python
 #     name: python3
-#   notebookname: App: Fourier (K3D + bokeh)
+#   notebookname: 'App: Fourier (K3D + bokeh)'
 # ---
 
 # %% [markdown]
