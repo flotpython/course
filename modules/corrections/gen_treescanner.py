e74b5ee553658337dc49b8dfb12c2d0f5e9baea9
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-10-26 15:50:55 +0100

    new exo treescanner

---
+++ modules/corrections/gen_treescanner.py
@@ -0,0 +1,66 @@
+from nbautoeval import (ExerciseGenerator, GeneratorArgs, 
+                        PPrintRenderer, PPrintCallRenderer)
+
+
+# @BEG@ name=treescanner
+def treescanner(tree):
+    """
+    enumerate all leaves in a tree
+    """
+    # a typical example where
+    # the 'yield from' statement 
+    # is the only way to go
+    if isinstance(tree, list):
+        for subtree in tree:
+            yield from treescanner(subtree)
+    else:
+        yield tree
+# @END@
+
+def tree(offset, width, depth):
+    """
+    one root, width sons, width**2 grandsons
+    """
+    def build(width, depth):
+        if depth == 0:
+            return [0]
+        else:
+            return [build(width, depth-1) for _ in range(width)]
+    def number_leaves(tree, value):
+        for i, subtree in enumerate(tree):
+            if not isinstance(subtree, list):
+                tree[i] = value
+                value += 1
+            else:
+                value = number_leaves(subtree, value)
+        return value
+    raw = build(width, depth)
+    number_leaves(raw, offset)
+    return raw
+
+T1 = [tree(1, 2, 2), tree(10, 3, 2), tree(20, 2, 3), 50, tree(100, 2, 4)]
+
+T2 = tree(1, 2, 5)
+
+
+treescanner_args = [
+    GeneratorArgs([], islice=(1000,)),
+    GeneratorArgs(0, islice=(1000,)),
+    GeneratorArgs([0], islice=(1000,)),
+    GeneratorArgs([[0]], islice=(1000,)),
+    GeneratorArgs([1, [2]], islice=(1000,)),
+    GeneratorArgs([[1, 2], 3, [4, 5]], islice=(1000,)),
+    GeneratorArgs(T1, islice=(1000,)),
+    GeneratorArgs(T2, islice=(1000,)),
+]
+
+# max_iterations is mostly a provision to avoid endless loops
+exo_treescanner = ExerciseGenerator(
+    treescanner, treescanner_args, max_iterations=100,
+    nb_examples=6,
+    result_renderer=PPrintRenderer(width=40),
+    call_renderer=PPrintCallRenderer(width=40),
+)
+
+print(tree(100, 2, 3))
+print(tree(100, 3, 2))
---
+++ w6/w6-s9-x6-treescanner.md
@@ -0,0 +1,57 @@
+---
+jupytext:
+  cell_metadata_filter: all
+  notebook_metadata_filter: all,-language_info,-toc,-jupytext.text_representation.jupytext_version,-jupytext.text_representation.format_version
+  text_representation:
+    extension: .md
+    format_name: myst
+kernelspec:
+  display_name: Python 3
+  language: python
+  name: python3
+notebookname: 'exercice: redirector'
+version: '3.0'
+---
+
+<div class="licence">
+<span>Licence CC BY-NC-ND</span>
+<span>Thierry Parmentelat &amp; Arnaud Legout</span>
+<span><img src="media/both-logos-small-alpha.png" /></span>
+</div>
+
++++
+
+## Exercice - niveau intermédiaire+
+
++++
+
+On veut écrire **une fonction génératrice** qui énumère tous les noeuds d'un arbre en le parcourant en profondeur d'abord.
+
+Pour simplifier l'exercice au maximum, nous ne considérons que des entrées constituées de listes et d'entiers.
+
+```{code-cell} ipython3
+from corrections.gen_treescanner import exo_treescanner
+exo_treescanner.example()
+```
+
++++ {"tags": []}
+
+la présentation de l'exemple peut laisser penser qu'il faut retourner une liste  
+mais ce n'est pas ce qui est demandé !  
+**attention** à bien implémenter **une fonction génératrice**
+
+```{code-cell} ipython3
+# à vous de jouer
+def treescanner(tree):
+    ...
+```
+
+```{code-cell} ipython3
+# pour le corriger
+
+exo_treescanner.correction(treescanner)
+```
+
+### indice
+
+rappelez-vous que pour parcourir un arbre en profondeur d'abord, un algorithme de parcours récursif est très adapté
