c5cd7c914aad66fc182f3bbb2802e0e1461fe752
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-11-09 10:42:39 +0100

    slightly revised version of postfix_eval_typed

--- modules/corrections/exo_postfix_eval.py
+++ modules/corrections/exo_postfix_eval.py
@@ -133,47 +133,37 @@ exo_postfix_eval = ExerciseFunction(
 
 
 # @BEG@ name=postfix_eval_typed latex_size=footnotesize
-def postfix_eval_typed(chaine, type):
+def postfix_eval_typed(chaine, result_type):
     """
     a postfix evaluator, using a parametric type
     that can be either `int`, `float` or `Fraction` or similars
     """
-    def divide(a, b):
-        if issubclass(type, int):
-            return a // b
-        else:
-            return a / b
-    stack = []
-    tokens = chaine.split()
-    for token in tokens:
-        operand = None
-        try:
-            operand = type(token)
-            stack.append(operand)
-        except ValueError:
-            operator = token
-            if len(stack) < 2:
-                # error: not enough values to operate on
-                return 'error-empty-stack'
-            right = stack.pop()
-            left = stack.pop()
-            if operator == '+':
-                stack.append(left + right)
-            elif operator == '-':
-                stack.append(left - right)
-            elif operator == '*':
-                stack.append(left * right)
-            elif operator == '/':
-                stack.append(divide(left, right))
-            else:
-                # error: unknown op
-                return 'error-syntax'
-    # we must have exactly one item in the stack
-    if len(stack) == 0:
-        return 'error-empty-stack'
-    elif len(stack) > 1:
-        return 'error-unfinished'
+    operators = {
+        '+': lambda x, y: x+y,
+        '-': lambda x, y: x-y,
+        '*': lambda x, y: x*y,
+        '/': lambda x, y: x//y if issubclass(result_type, int) else x/y,
+    }
 
+    stack = []
+    for token in chaine.split():
+        if token in operators:
+            # compute operation on last 2 entries
+            try:
+                rhs = stack.pop()
+                lhs = stack.pop()
+            except:
+                return "error-empty-stack"
+            result = operators[token](lhs, rhs)
+            stack.append(result)
+        else:
+            try:
+                stack.append(result_type(token))
+            except:
+                return 'error-syntax'
+            # parse as int and stack up
+    if len(stack) != 1:
+        return 'error-unfinished'
     return stack.pop()
 # @END@
 
